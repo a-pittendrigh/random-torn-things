3d9006394be812025b74f06471b774691dbad40e
Alexander Pittendrigh <a.pittendrigh@gmail.com>
2022-07-21 19:22:11 +0100

    add item names

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const purchases = ["16:17:45 - 21/07/22 You bought 4x Feathery Hotel Coupon on Nuvak's bazaar at $13,545,000 each for a total of $54,180,000"];
-const mug = "16:18:00 - 21/07/22 You used 25 energy attacking Nuvak and mugged them for $12,711,972 (chain #1) [view]";
+// const purchases = ["16:17:45 - 21/07/22 You bought 4x Feathery Hotel Coupon on Nuvak's bazaar at $13,545,000 each for a total of $54,180,000"];
+// const mug = "16:18:00 - 21/07/22 You used 25 energy attacking Nuvak and mugged them for $12,711,972 (chain #1) [view]";
+
+const mug = "11:08:25 - 21/07/22 You used 25 energy attacking TheTollTroll and mugged them for $9,556,339 (chain #1) [view]";
+const purchases = [
+"11:08:14 - 21/07/22 You bought 1x Type 98 Anti Tank on TheTollTroll's bazaar at $18,000,000 each for a total of $18,000,000",
+"11:08:07 - 21/07/22 You bought 1x Type 98 Anti Tank on TheTollTroll's bazaar at $18,500,000 each for a total of $18,500,000",
+"11:08:02 - 21/07/22 You bought 24x Lottery Voucher on TheTollTroll's bazaar at $1,050,000 each for a total of $25,200,000",
+"11:07:53 - 21/07/22 You bought 144x LSD on TheTollTroll's bazaar at $52,500 each for a total of $7,560,000",
+"11:07:49 - 21/07/22 You bought 490x Tear Gas on TheTollTroll's bazaar at $64,500 each for a total of $31,605,000"]
 
 const valuesOfPurchases = (purchases) => {
     return purchases.map(purchase => {
@@ -8,7 +16,9 @@ const valuesOfPurchases = (purchases) => {
         const startOfAmount = purchase.indexOf('$');
         const endOfAmount = purchase.indexOf('each');
         const amount = purchase.substring(startOfAmount, endOfAmount - 1);
+        const nameOfItem = purchase.substring(purchase.indexOf('x') + 2, purchase.indexOf(' on '))
         return {
+            nameOfItem,
             quantity,
             amount: asInt(amount)
         }
